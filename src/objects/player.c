c582b68c20a367601141e83f12a04f9e021381f3
Timothy Tripp <ti.n.tripp@baypath.net>
2026-01-31 17:28:58 -0500

    Super messy plr update loop--refactor code; + basic controller support

--- src/objects/player.c
+++ src/objects/player.c
@@ -9,6 +9,7 @@ void player_init(Player *player){
     player->sprite = LoadTexture("res/images/onionboy.png");
     player->dir = PLRDIR_RIGHT;
 
+    player->anim_len = 2;
     player->anim_frame = 0;
     player->frame_offx = 4;
     player->frame_offy = 15;
@@ -18,22 +19,58 @@ void player_init(Player *player){
     player->velocity = (Vector2){ 0, 0 };
     player->hitbox = (Rectangle){ 80, 30, 8, 8 }; 
     player->speed = 6;
+
+    //TODO: gamepads should be their own thing, 
+    // in DR2C they ARE their own obj and select a player to control
+    player->gamepad = 0;
 }
 void player_free(Player *player){
     UnloadTexture(player->sprite);
 }
 
 void player_update(Player *player, TileMap *tilemap){
-    if (IsKeyPressed(KEY_W) && player_is_on_ground(player, tilemap))
-        player->velocity.y = -190;
+    bool jump_pressed = IsGamepadAvailable(player->gamepad)?
+        IsGamepadButtonPressed(player->gamepad, GAMEPAD_BUTTON_RIGHT_FACE_DOWN)
+        : IsKeyPressed(KEY_W);
+        
+    bool jump_down = IsGamepadAvailable(player->gamepad)?
+        IsGamepadButtonDown(player->gamepad, GAMEPAD_BUTTON_RIGHT_FACE_DOWN)
+        : IsKeyDown(KEY_W);
+        
+    bool jump_released = IsGamepadAvailable(player->gamepad)?
+        IsGamepadButtonReleased(player->gamepad, GAMEPAD_BUTTON_RIGHT_FACE_DOWN)
+        : IsKeyReleased(KEY_W);
+
+    if (jump_pressed && (
+        player_is_on_ground(player, tilemap) || 
+            (
+                player_is_on_wall(player, tilemap, 3) && 
+                !player_is_on_ground(player, tilemap)
+            )
+        )
+    ){
+        player->velocity.y = -180;
+
+        if (
+            player_is_on_wall(player, tilemap, 3) && 
+            !player_is_on_ground(player, tilemap)
+        ){
+            player->velocity.x = -player->dir * 200;
+        }
+    }
+
+    player->anim_len = 2;
     
     // change to check if state == jumping
-    if (IsKeyReleased(KEY_W) && player->velocity.y < -50) 
+    if (jump_released && player->velocity.y < -50) 
         player->velocity.y = -50;
 
-    player->velocity.x += (
-        IsKeyDown(KEY_D) - IsKeyDown(KEY_A)
-    ) * player->speed;
+
+    float move_x = IsKeyDown(KEY_D) - IsKeyDown(KEY_A);
+    if (IsGamepadAvailable(player->gamepad)){
+        move_x = GetGamepadAxisMovement(player->gamepad, GAMEPAD_AXIS_LEFT_X);
+    }
+    player->velocity.x += move_x * player->speed;
     player->velocity.x *= 0.95;
 
     player->anim_frame += abs((int)player->velocity.x) * GetFrameTime() / 8;
@@ -41,11 +78,17 @@ void player_update(Player *player, TileMap *tilemap){
     if (player->velocity.x > 0) player->dir = PLRDIR_RIGHT;
 
     //gravity
-    // if jumping grav lower
     int grav = 1000;
-    if (IsKeyDown(KEY_W) && player->velocity.y < 0)
-    grav = 360;
-    player->velocity.y += grav * GetFrameTime();
+    // if jumping grav lower
+    if (jump_down && player->velocity.y < 0){
+        grav = 360;
+    }
+    if (player_is_on_wall(player, tilemap, 1)){
+        // make plr fall linearly
+        player->velocity.y = grav * 2 * GetFrameTime();
+    }else
+        // apply gravity like usual
+        player->velocity.y += grav * GetFrameTime();
     
     player_move(player, tilemap, Vector2Scale(player->velocity, GetFrameTime()));
 }
@@ -53,8 +96,9 @@ void player_draw(Player *player){
     if (!round(player->velocity.x)){
         player->anim_frame = 0;
     }
+
     //soon 2 will be anim length -- make json parsing work
-    int frame = ((int)player->anim_frame % 2); 
+    int frame = ((int)player->anim_frame % player->anim_len); 
 
     DrawTextureRec(
         player->sprite, 
@@ -74,10 +118,9 @@ void player_draw(Player *player){
     int right = (player->hitbox.x + player->hitbox.width) / TILE_SIZE;
     int top = player->hitbox.y / TILE_SIZE;
     int bottom = (player->hitbox.y + player->hitbox.height) / TILE_SIZE;
-
-    //DrawRectangleLines(left * TILE_SIZE, top * TILE_SIZE, (right - left + 1) * TILE_SIZE, (bottom - top + 1) * TILE_SIZE, GREEN);
+    DrawRectangleLines(left * TILE_SIZE, top * TILE_SIZE, (right - left + 1) * TILE_SIZE, (bottom - top + 1) * TILE_SIZE, GREEN);
     
-    DrawRectangleRec(player->hitbox, PINK);
+    //DrawRectangleRec(player->hitbox, PINK);
 }
 
 bool player_is_colliding(Player *player, TileMap *tilemap){
@@ -103,11 +146,25 @@ bool player_is_colliding(Player *player, TileMap *tilemap){
     return collided;
 }
 
+bool player_collide_at(Player *player, TileMap *tilemap, int x, int y){
+    player->hitbox.x += x;
+    player->hitbox.y += y;
+
+    bool collide = player_is_colliding(player, tilemap);
+
+    player->hitbox.x -= x;
+    player->hitbox.y -= y;
+
+    return collide;
+}
+
 bool player_is_on_ground(Player *player, TileMap *tilemap){
-    ++player->hitbox.y;
-    bool on_ground = player_is_colliding(player, tilemap);
-    --player->hitbox.y;
-    return on_ground;
+    return player_collide_at(player, tilemap, 0, 1);
+}
+
+bool player_is_on_wall(Player *player, TileMap *tilemap, int reach){
+    return player_collide_at(player, tilemap, player->dir * reach, 0);
+    
 }
 
 void player_move_axis(
--- src/objects/player.h
+++ src/objects/player.h
@@ -10,16 +10,19 @@ typedef enum PlayerDirection {
 typedef struct Player Player;
 
 struct Player{
-    Texture sprite;
+    Texture2D sprite;
     Rectangle hitbox;
     Vector2 velocity;
     Vector2 remainder;
 
+    // the controller id that controls the player (none = -1)
+    int gamepad; 
+    float speed;
+
     PlayerDirection dir;
+    int anim_len;
     float anim_frame;
     int frame_offx, frame_offy, frame_w, frame_h;
-
-    float speed;
 };
 
 void player_init(Player *player);
@@ -30,3 +33,4 @@ void player_draw(Player *player);
 
 void player_move(Player *player, TileMap *tiles, Vector2 dv);
 bool player_is_on_ground(Player *player, TileMap *tilemap); 
+bool player_is_on_wall(Player *player, TileMap *tilemap, int reach); 
